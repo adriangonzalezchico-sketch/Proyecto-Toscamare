d9f330e2d9b8b54c0c93a67a04ddd64be3821114
Adrian Gonzalez <adrian.gonzalezchico@riberadeltajo.es>
2026-01-26 09:55:46 +0100

    configuración inicial del proyecto y limpieza, instalacion de tailwind y comprobar su funcionamiento

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+//aqui arriba los imports necesarios
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind funciona 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
